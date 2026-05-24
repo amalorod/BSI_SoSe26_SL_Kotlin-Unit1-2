cab103e94fa352ce17d48bcfb014c26585bb1954
amalorod <alicmalorodow@gmail.com>
2026-05-24 20:21:37 +0200

    Dice Roller App - zwischenspeicher 2

--- diceroller/src/main/java/com/example/diceroller/MainActivity.kt
+++ diceroller/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -14,6 +15,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroller.ui.theme.DiceRollerTheme
@@ -41,7 +43,15 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier.fillMaxSize().wrapConte
     Column(modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Image(
 
+            painter = painterResource(R.drawable.dice_1),
+            contentDescription = "1"
+        )
+        Button(onClick = { /*TODO*/ }) {
+            Text(stringResource(R.string.roll))
+        }
+    }
         Button(onClick = { /*TODO*/ }) {
             Text(stringResource(R.string.roll))
 
