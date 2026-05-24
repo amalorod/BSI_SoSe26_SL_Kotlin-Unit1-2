43505809b7e2d5ff1c461c9082baaf9473007c4b
amalorod <alicmalorodow@gmail.com>
2026-05-24 22:22:38 +0200

    Dice Roller App - beendet

--- diceroller/src/main/java/com/example/diceroller/MainActivity.kt
+++ diceroller/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,18 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,36 +33,48 @@ class MainActivity : ComponentActivity() {
             DiceRollerTheme {
                 DiceRollerApp()
             }
-
-
-                }
-            }
         }
+    }
+}
 
 
 
 
 
 @Composable
-fun DiceWithButtonAndImage(modifier: Modifier = Modifier.fillMaxSize().wrapContentSize(Alignment.Center)) {
-    Column(modifier = modifier,
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result by remember { mutableIntStateOf(1) }
+
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
-
-            painter = painterResource(R.drawable.dice_1),
-            contentDescription = "1"
+            painter = painterResource(imageResource),
+            contentDescription = result.toString()
         )
-        Button(onClick = { /*TODO*/ }) {
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = { result = (1..6).random() }) {
             Text(stringResource(R.string.roll))
         }
     }
-        Button(onClick = { /*TODO*/ }) {
-            Text(stringResource(R.string.roll))
 
 
 
-    }
+
+
 
 
 
